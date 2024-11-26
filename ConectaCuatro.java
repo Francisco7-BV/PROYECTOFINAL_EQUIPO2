142f344dcc5e4aceaf02f1caf5638ac65bebe5c9
fbarretovelazquez7 <fbarretovelazquez7@ciencias.unam.mx>
2024-11-26 13:29:14 -0600

    Edicion clase ConectaCuatro

--- ConectaCuatro.java
+++ ConectaCuatro.java
@@ -97,12 +97,12 @@ public class ConectaCuatro{
      * Metodo que verifica si se han conectado 4 fichas con simbolos iguales horizontalmente
      *
      */
-
+ 
     boolean horizontal;
       public boolean verificacionHorizontal(){
-          for (int i=5; i>=0; i--){
+          for (int i=0; i<=5; i++){
     	    for (int j=0; j<=3; j++){
-    		if ((tablero[i][j] == tablero[i+1][j+2]) && (tablero [i][j] == tablero[i+2][j+2]) && (tablero [i][j] == tablero[i+3][j+3])){
+    		if ((tablero[i][j] == tablero[i][j+1]) && (tablero [i][j] == tablero[i][j+2]) && (tablero [i][j] == tablero[i][j+3])){
 		    horizontal = true;
 		} else
 		    { horizontal = false;
@@ -115,7 +115,126 @@ public class ConectaCuatro{
 	  } else
 	      return false;
       }
- 
+
+    /**
+     * Metodo que verifica si se han conectado 4 fichas con simbolos iguales verticalmente
+     *
+     */
+    
+        boolean vertical;
+      public boolean verificacionVertical(){
+          for (int i=0; i<=6; i++){
+            for (int j=0; j<=2; j++){
+                if ((tablero[j][i] == tablero[j+1][i]) && (tablero [j][i] == tablero[j+2][i]) && (tablero [j][i] == tablero[j+3][i])){
+                    vertical = true;
+                } else
+                    { vertical = false;
+                    }
+            }
+        }
+
+          if (vertical == true) {
+              return true;
+          } else
+              return false;
+      }
+
+    /**
+     * Metodo que verifica si se han conectado 4 fichas con simbolos iguales dioagonalmente a 135°
+     *
+     */
+
+    public boolean verificacionDiagonal135(){
+	if  (
+	     ((tablero[2][0] == tablero[3][1]) && (tablero[2][0] == tablero[4][2]) && (tablero [2][0] == tablero[5][3])) ||
+	     ((tablero[1][0] == tablero[2][1]) && (tablero[1][0] == tablero[3][2]) && (tablero [2][0] == tablero[4][3])) ||
+	     ((tablero[2][1] == tablero[3][2]) && (tablero[2][1] == tablero[4][3]) && (tablero [2][1] == tablero[5][4])) ||
+	     ((tablero[0][0] == tablero[1][1]) && (tablero[0][0] == tablero[2][2]) && (tablero [0][0] == tablero[3][3])) ||
+	     ((tablero[1][1] == tablero[2][2]) && (tablero[1][1] == tablero[3][3]) && (tablero [1][1] == tablero[4][4])) ||
+	     ((tablero[2][2] == tablero[3][3]) && (tablero[2][2] == tablero[4][4]) && (tablero [2][2] == tablero[5][5])) ||
+	     ((tablero[0][1] == tablero[1][2]) && (tablero[0][1] == tablero[2][3]) && (tablero [0][1] == tablero[3][4])) ||
+	     ((tablero[1][2] == tablero[2][3]) && (tablero[1][2] == tablero[3][4]) && (tablero [1][2] == tablero[4][5])) ||
+	     ((tablero[2][3] == tablero[3][4]) && (tablero[2][3] == tablero[4][5]) && (tablero [2][3] == tablero[5][6])) ||
+	     ((tablero[0][2] == tablero[1][3]) && (tablero[0][2] == tablero[2][4]) && (tablero [0][2] == tablero[3][5])) ||
+	     ((tablero[1][3] == tablero[2][4]) && (tablero[1][3] == tablero[3][5]) && (tablero [1][3] == tablero[4][6])) ||
+	     ((tablero[0][3] == tablero[1][4]) && (tablero[0][3] == tablero[2][5]) && (tablero [0][3] == tablero[3][6]))
+	     ){
+
+	    return true;
+	} else
+	    {
+	    return false;
+	    }
+    }
+    
+    /**
+     * Metodo que verifica si se han conectado 4 fichas con simbolos iguales dioagonalmente a 45°
+     *
+     */
+
+        public boolean verificacionDiagonal45(){                                                                                                                                                       
+        if  (                                                                                                                                                                                   
+             ((tablero[0][3] == tablero[1][2]) && (tablero[0][3] == tablero[2][1]) && (tablero [0][3] == tablero[3][0])) ||       
+             ((tablero[0][4] == tablero[1][3]) && (tablero[0][4] == tablero[2][2]) && (tablero [0][4] == tablero[3][1])) ||   
+             ((tablero[1][3] == tablero[2][2]) && (tablero[1][3] == tablero[3][1]) && (tablero [1][3] == tablero[4][0])) ||  
+             ((tablero[0][5] == tablero[1][4]) && (tablero[0][5] == tablero[2][3]) && (tablero [0][5] == tablero[3][2])) ||  
+             ((tablero[1][4] == tablero[2][3]) && (tablero[1][4] == tablero[3][2]) && (tablero [1][4] == tablero[4][1])) ||  
+             ((tablero[2][3] == tablero[3][2]) && (tablero[2][3] == tablero[4][1]) && (tablero [2][3] == tablero[5][0])) ||     
+             ((tablero[0][6] == tablero[1][5]) && (tablero[0][6] == tablero[2][4]) && (tablero [0][6] == tablero[3][3])) ||   
+             ((tablero[1][5] == tablero[2][4]) && (tablero[1][5] == tablero[3][3]) && (tablero [1][5] == tablero[4][2])) ||        
+             ((tablero[2][4] == tablero[3][3]) && (tablero[2][4] == tablero[4][2]) && (tablero [2][4] == tablero[5][1])) ||     
+             ((tablero[1][6] == tablero[2][5]) && (tablero[1][6] == tablero[3][4]) && (tablero [1][6] == tablero[4][3])) ||     
+             ((tablero[2][5] == tablero[3][4]) && (tablero[2][5] == tablero[4][3]) && (tablero [2][5] == tablero[5][2])) ||                
+             ((tablero[2][6] == tablero[3][5]) && (tablero[2][6] == tablero[4][4]) && (tablero [2][6] == tablero[5][3])) 
+             ){                                                                                                                                                                               
+                            
+            return true;                                   
+        } else   
+            {      
+            return false;        
+            }                                                                                              
+    }
+
+    /**
+     * Metodo que determina si un jugador ha ganado (Ha colocado 4 fichas con el ismo simbolo juntas) (En cualquier direccion)
+     *
+     */
+
+    public boolean haGanado(){
+	if ((this.verificacionDiagonal45() == true) || (this.verificacionDiagonal135() == true) || (this.verificacionHorizontal() == true) || (this.verificacionVertical() == true)){
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+
+    /**
+     * Metodo que determina si el tablero esta lleno
+     *
+     */
+
+    public boolean tableroLleno(){
+	if ((tablero [0][0] != ' ') && (tablero [0][1] != ' ') && (tablero [0][2] != ' ') && (tablero [0][3] != ' ') && (tablero [0][4] != ' ') && (tablero [0][5] != ' ') &&
+ 	    (tablero [0][6] != ' ') &&
+	    (tablero [1][0] != ' ') && (tablero [1][1] != ' ') && (tablero [1][2] != ' ') && (tablero [1][3] != ' ') && (tablero [1][4] != ' ') && (tablero [1][5] != ' ') &&
+            (tablero [1][6] != ' ') &&
+	    (tablero [2][0] != ' ') && (tablero [2][1] != ' ') && (tablero [2][2] != ' ') && (tablero [2][3] != ' ') && (tablero [2][4] != ' ') && (tablero [2][5] != ' ') &&
+            (tablero [2][6] != ' ') &&
+	    (tablero [3][0] != ' ') && (tablero [3][1] != ' ') && (tablero [3][2] != ' ') && (tablero [3][3] != ' ') && (tablero [3][4] != ' ') && (tablero [3][5] != ' ') &&
+            (tablero [3][6] != ' ') &&
+	    (tablero [4][0] != ' ') && (tablero [4][1] != ' ') && (tablero [4][2] != ' ') && (tablero [4][3] != ' ') && (tablero [4][4] != ' ') && (tablero [4][5] != ' ') &&
+            (tablero [4][6] != ' ') &&
+	    (tablero [5][0] != ' ') && (tablero [5][1] != ' ') && (tablero [5][2] != ' ') && (tablero [5][3] != ' ') && (tablero [5][4] != ' ') && (tablero [5][5] != ' ') &&
+            (tablero [5][6] != ' ') &&
+	    (tablero [6][0] != ' ') && (tablero [6][1] != ' ') && (tablero [6][2] != ' ') && (tablero [6][3] != ' ') && (tablero [6][4] != ' ') && (tablero [6][5] != ' ') &&
+            (tablero [6][6] != ' ')
+	    ) {
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+    
     
 public static void main (String [] args){
     ConectaCuatro prueba = new ConectaCuatro();
